18aaf0fc1dc185d93eede6fa88115b1039d5490d
giannai25 <146111864+giannai25@users.noreply.github.com>
2023-12-14 17:18:07 +0000

    first commit

---
+++ StuffRunner.java
@@ -0,0 +1,8 @@
+public class StuffRunner {
+    public static void main(String[]args){
+        Stuff stuff=new Stuff(10);
+        stuff.printAddAmount(6);
+        System.out.println(stuff.getValue());
+        }
+    
+}
